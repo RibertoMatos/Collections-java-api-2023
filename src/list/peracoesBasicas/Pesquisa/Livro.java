4eb59c761c3a6b6576627cc5b5e72ab88f7adcab
RibertoMatos <riba_matos@yahoo.com.br>
2024-09-21 19:55:37 -0300

    Aula de listas Catalago de livros
    Signed-off-by: RibertoMatos <riba_matos@yahoo.com.br>

---
+++ src/list/peracoesBasicas/Pesquisa/Livro.java
@@ -0,0 +1,37 @@
+package list.peracoesBasicas.Pesquisa;
+
+public class Livro {
+    //atributoos
+    private String titulo;
+    private String autor;
+    private int anoPublicacao;
+
+    //alt + insert para contructor
+
+    public Livro(String titulo, String autor, int anoPublicacao) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", anoPublicacao=" + anoPublicacao +
+                '}';
+    }
+}
